9f6b51e824535b27c6bd70424eb4c3699fa1aba3
hussein <artistpenman1515@gmail.com>
2025-01-10 07:32:13 -0800

    lesson two added

--- Source.cpp
+++ Source.cpp
@@ -1,10 +1,26 @@
 #include <format>
 #include <iostream>
+#include <string>
 using std::format;
 using std::cout;
 
-
+//void func() {
+//	static auto i = 10;
+//	cout << format("refrence to number {}\n", ++i);
+//}
+void func(const std::string& s) {
+	static auto i = 10;
+	cout << format("refrence to string is {}\n", s);
+}
 int main() {
-	cout << "lesson two started";
-	return 0;
+	//cout << "lesson two started";
+	std::string s {"this is a long string as a exmpale"};
+	cout << format("value is {}\n" , s);
+	func(s);
+	//func();
+	//func();
+	//func();
+	//func();
+	//func();
+	//func();
 }
