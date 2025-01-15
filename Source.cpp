7cc5217c4f1697f5fe3add8f986caac653d80c75
hussein <artistpenman1515@gmail.com>
2025-01-15 00:47:32 -0800

    io and io stream

--- Source.cpp
+++ Source.cpp
@@ -1,62 +1,57 @@
 ﻿
-
-#include <format>
+#include <iostream>
 #include <string>
-#include <string_view>
-#include <vector>
-#include <numbers>
-
-using std::format;
-using std::string;
-using std::vector;
-using std::numbers::pi;
-
-template<typename T>
-struct Frac {
-    T n;
-    T d;
-};
-
-// format specialization
-template <typename T>
-struct std::formatter<Frac<T>> : std::formatter<unsigned> {
-    template <typename Context>
-    auto format(const Frac<T>& f, Context& ctx) const {
-        return format_to(ctx.out(), "{}/{}", f.n, f.d);
-    }
-};
-
-// format print
-template<typename... Args>
-constexpr void print(const std::string_view str_fmt, Args&&... args) {
-    fputs(std::vformat(str_fmt, std::make_format_args(args...)).c_str(), stdout);
-}
+#include <iomanip>
 
 int main() {
-    const int inta{ 47 };
-    const char* human{ "earthlings" };
-    const string alien{ "vulcans" };
-    const double dpi{ pi };
+    std::cout << "Hello, World!\n";
 
-    print("inta is {}\n", inta);
-    print("Hello {}\n", human);
+    std::string istr{};
+    std::cout << "Prompt: ";
+    std::cin >> istr;    // one word at a time
+    std::cout << "Input: " << istr << '\n';
 
-    print("Hello {} we are {}\n", human, alien);
-    print("Hello {1} we are {0}\n", human, alien);
+    char buf[128]{};
+    std::cout << "Prompt: ";
+    std::cin.getline(buf, sizeof(buf));
+    std::cout << "Input: " << buf << '\n';
 
-    print("π is {}\n", dpi);
-    print("π is {:.5}\n", dpi);
-    print("inta is {1:}, π is {0:.5}\n", dpi, inta);
+    // integer formatting
+    int i1{ 42 };
+    int i2{ 127 };
+    int i3{ 5555 };
+    std::cout << "default: " << i1 << ' ' << i2 << ' ' << i3 << '\n';
+    std::cout << "hex: " << std::hex << i1 << ' ' << i2 << ' ' << i3 << '\n';
+    std::cout << "hex with showbase: " << std::showbase << std::hex << i1 << ' ' << i2 << ' ' << i3 << '\n';
+    std::cout << "octal with showbase: " << std::oct << i1 << ' ' << i2 << ' ' << i3 << '\n';
+    std::cout << "default: " << std::noshowbase << std::dec << i1 << ' ' << i2 << ' ' << i3 << "\n\n";
 
-    print("inta is [{:*<10}]\n", inta);
-    print("inta is [{:0>10}]\n", inta);
-    print("inta is [{:^10}]\n", inta);
-    print("inta is [{:_^10}]\n", inta);
+    // floating point formatting options
+    double d1{ 3.1415926534 };
+    double d2{ 1234.5 };
+    double d3{ 4.2e-10 };
+    std::cout << "default: " << d1 << ' ' << d2 << ' ' << d3 << ' ' << '\n';
+    std::cout << "fixed: " << std::fixed << d1 << ' ' << d2 << ' ' << d3 << ' ' << '\n';
+    std::cout << "scientific: " << std::scientific << d1 << ' ' << d2 << ' ' << d3 << ' ' << '\n';
+    std::cout << "set precision (3): " << std::setprecision(3) << std::fixed << d1 << ' ' << d2 << ' ' << d3 << ' ' << '\n';
+    std::cout << "scientific (7): " << std::setprecision(7) << std::scientific << d1 << ' ' << d2 << ' ' << d3 << ' ' << '\n';
+    std::cout.unsetf(std::ios_base::floatfield);
+    std::cout << "default: " << d1 << ' ' << d2 << ' ' << d3 << ' ' << std::endl << '\n';
 
-    print("{:>8}: [{:04X}]\n", "Hex", inta);
-    print("{:>8}: [{:4o}]\n", "Octal", inta);
-    print("{:>8}: [{:4d}]\n", "Decimal", inta);
+    // string formatting options
+    std::string s1{ "This is a string." };
+    std::string s2{ "This is a much longer string." };
+    std::string s3{ "Today's news: Big Light in Sky Slated to Appear in East" };
 
-    Frac<long> n{ 3, 5 };
-    print("Frac: {}\n", n);
+    std::cout << s1 << '\n';
+    std::cout << s2 << '\n';
+    std::cout << s3 << '\n';
+
+    std::cout << std::setw(64) << std::right << s1 << std::endl;
+    std::cout << std::setw(64) << std::right << s2 << std::endl;
+    std::cout << std::setw(64) << std::right << s3 << std::endl;
+
+    std::cout << std::setfill('-') << std::setw(64) << std::right << s1 << std::endl;
+    std::cout << std::setfill(' ') << std::setw(64) << std::right << s1 << std::endl;
+    std::cout << std::left << s1 << std::endl;
 }
